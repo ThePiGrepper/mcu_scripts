9474296b4cc90bd2ae705e5486d59d0a29620016
Erick Cafferata <lcafferatac@gmail.com>
2018-02-04 22:36:40 -0500

    adds buildfile relative path correction
    situation now:
    stdlib="../../stdlibrelativepath" N=1(one level below $BSYNC_PATH
    expected:
    stdlib="{../,N}../stdlibRELPATH", where N is depth relative to $BSYNC_PATH
    
    Design Steps:
    - Find N. N MUST BE > 0. If N==0, error.
    - To filter multiple consecutive '/', use 'readlink -f <path>' to 'normalize' the path strings.
    - Then extract the difference using: ${newpath#${BSYNC_PATH}}. if it returns different from
      newpath, then it's OK; it's NOT a valid path otherwise.
    - count the number of '/' characters in the previous output, that is equal to the depth(N):
      - to do this, count the number of characters for the whole string: ${#var}.
      - then, remove all '/' characters from the string and count again. ${var//\/}.
      - the difference between both counts is N. $((count - count2))
    - replace current '../' with N '../' concatenated strings to each path in ninja.build.
    - To  concatanate a string, this cool hack: printf "%${N}s" | sed 's# #..\\/#g'
    - finally, revert the one for '../obj' directory.
    * Current solution does not work if <newpath> is inside a symbolic link. readlink will resolve
      the link, and it won't match with BSYNC_PATH, aborting operation.

--- mcu_createProject.sh
+++ mcu_createProject.sh
@@ -3,6 +3,30 @@
 #mcu_createProject creates a new folder named 'projectName'
 #with the MCU project structure inside.
 #usage: ./mcu_createProject <projectName> <mcu_type>
+
+#Changes the content of build.ninja external references(linker script,external libs)
+#to match the current IN-TREE directory(under $BSYNC_PATH, otherwise no change is made).
+chRelPath()
+{
+  #calculate directory depth from BSYNC_PATH
+  local bsyncpath=$(readlink -f ${BSYNC_PATH})
+  local newpath=$(readlink -f $1)
+  local diffpath=${newpath#${bsyncpath}}
+  if [[ ${newpath} == ${diffpath} ]] ; then
+    echo 'Project directory is not under current $BSYNC_PATH path. abort'
+    return 1
+  fi
+  local diffcount=${#diffpath}
+  local strippeddiffpath=${diffpath//\/}
+  local strippeddiffcount=${#strippeddiffpath}
+  local depth=$((diffcount-strippeddiffcount))
+  #append '../', depth times
+  local addstr=$(printf "%${depth}s" | sed 's# #../#g')
+  local substr=$(printf "%${depth}s" | sed 's# #\\.\\./#g')
+  sed -i -e "s#\.\./#${addstr}#" -e "s#${substr}obj#../obj#" ${1}/src/build.ninja
+  return 0
+}
+
 if test $# -ne 2 ; then
   echo -e "Usage: mcu_createProject <projectName> <mcu_type>\n\
 Incorrect Parameter count($#).abort"
@@ -30,10 +54,15 @@ if test $? -eq 1 ; then
   echo "$mcu_type is not a valid tag. abort"
   exit 1
 fi
-mkdir $projectName
+mkdir $projectName 2>/dev/null
 if test $? -eq 1; then
   echo "$projectName folder couldn't be created. abort"
   exit 1
 fi
 git -C $BSYNC_PATH/simple_test archive $mcu_type | tar Cx $projectName
+chRelPath $projectName
+if test $? -eq 1 ; then
+  rm -rf $projectName
+  exit 1
+fi
 echo "Project Folder '$projectName' created successfully for $mcu_type"
